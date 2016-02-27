c7c3f3fb357e9918f6a46e8a787f16108c16e814
lokiteitor <lokiteitor513@gmail.com>
2016-02-27 16:25:13 -0600

    Correcciones en composer

--- Install_Server_Raspberry/install.sh
+++ Install_Server_Raspberry/install.sh
@@ -46,7 +46,11 @@ if [ "$user" == "root" ]; then
     curl -sL https://deb.nodesource.com/setup_4.x | bash -
     apt-get install -y nodejs
     # Instalando Composer
+    php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+    php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
     php composer-setup.php --install-dir=/usr/local/bin/composer --filename=composer
+    php -r "unlink('composer-setup.php');"
+
     # Instalacion de bower
     npm install -g bower
 
